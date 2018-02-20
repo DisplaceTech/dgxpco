b148ee162ae3377fd8f4f8a02c8b2c3512039576
Steve Grunwell <steve@stevegrunwell.com>
2018-02-20 02:03:35 +0000

    Scaffold tests for the WP core test suite via WP-CLI

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tests for some file or class.
+ *
+ * @package DisplaceTech\DGXPCO
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
